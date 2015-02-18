9dcd89d1bde5fea529531cf70f121a8c70d31a13
Ruud Jonk <ruud@multisafepay.com>
2015-02-18 13:43:26 +0100

    Sofort plugin for X-Cart 5.

---
+++ XCart/classes/XLite/Module/MultiSafepay/Sofort/Controller/Customer/Sofort.php
@@ -0,0 +1,32 @@
+<?php
+// vim: set ts=4 sw=4 sts=4 et:
+
+namespace XLite\Module\MultiSafepay\Sofort\Controller\Customer;
+
+class Sofort extends \XLite\Controller\Customer\ACustomer
+{
+    /**
+     *
+     * @return void
+     */
+    protected function doActionTransaction()
+    {
+        try {
+
+            $processor = new \XLite\Module\MultiSafepay\Sofort\Model\Payment\Processor\Sofort();
+
+            $processor->doTransactionRequest(
+                \XLite\Core\Request::getInstance()->iid,
+                \XLite\Core\Request::getInstance()->transid
+            );
+
+        } catch (\Exception $e) {
+
+            \XLite\Core\TopMessage::addError(
+                static::t('An error occured processing your transaction request, please try again using another payment method.')
+            );
+
+            $this->setReturnURL('checkout');
+        }
+    }
+}
