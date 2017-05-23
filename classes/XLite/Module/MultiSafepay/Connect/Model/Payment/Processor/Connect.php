9b6e489b02a83b7cc1860a13039002df088bbbca
Ruud Jonk <ruud@multisafepay.com>
2017-05-23 14:43:37 +0200

    Restructured. Added disclaimers

---
+++ classes/XLite/Module/MultiSafepay/Connect/Model/Payment/Processor/Connect.php
@@ -0,0 +1,549 @@
+<?php
+
+/**
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the MultiSafepay plugin
+ * to newer versions in the future. If you wish to customize the plugin for your
+ * needs please document your changes and make backups before you update.
+ *
+ * @category    MultiSafepay
+ * @package     Connect
+ * @author      TechSupport <techsupport@multisafepay.com>
+ * @copyright   Copyright (c) 2017 MultiSafepay, Inc. (http://www.multisafepay.com)
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace XLite\Module\MultiSafepay\Connect\Model\Payment\Processor;
+
+class Connect extends \XLite\Model\Payment\Base\WebBased
+{
+
+    public $settings = 'MultiSafepay Connect';
+    public $icon = 'msp_connect.png';
+    public $gateway = 'Connect';
+    public $transactionid = '';
+
+    /**
+     * Get operation types
+     *
+     * @return array
+     */
+    public function getOperationTypes()
+    {
+        return array(
+            self::OPERATION_SALE,
+        );
+    }
+
+    /**
+     * Get allowed backend transactions
+     *
+     * @return string Status code 
+     */
+    public function getAllowedTransactions()
+    {
+        return array(
+            \XLite\Model\Payment\BackendTransaction::TRAN_TYPE_REFUND
+        );
+    }
+
+    /**
+     * Get settings widget or template
+     *
+     * @return string Widget class name or template path
+     */
+    public function getSettingsWidget()
+    {
+        return 'modules/MultiSafepay/Connect/config.twig';
+    }
+
+    /**
+     * Process return
+     *
+     * @param \XLite\Model\Payment\Transaction $transaction Return-owner transaction
+     *
+     * @return void
+     */
+    public function processReturn(\XLite\Model\Payment\Transaction $transaction)
+    {
+        require_once LC_DIR_MODULES . 'MultiSafepay' . LC_DS . 'API' . LC_DS . 'Autoloader.php';
+        parent::processReturn($transaction);
+
+        $message = '';
+        $data = array();
+        try {
+            if (\XLite\Core\Request::getInstance()->transactionid) {
+                $status = $transaction::STATUS_FAILED;
+
+                $settings = $this->getPaymentSettings($this->settings);
+                if ($this->getSetting('transaction_type') == '1') {
+                    $order_id = \XLite\Core\Request::getInstance()->txnId;
+                } else {
+                    $order_id = \XLite\Core\Request::getInstance()->transactionid;
+                }
+
+                $msp = new \MultiSafepayAPI\Client();
+                $msp->setApiKey($this->getSetting('api_key'));
+                $msp->setApiUrl($this->getEnvironment());
+
+                $response = $msp->orders->get('orders', $order_id);
+
+                switch ($response->status) {
+                    case "initialized":
+                        $order_status = $transaction::STATUS_PENDING;
+                        break;
+                    case "completed":
+                        $order_status = $transaction::STATUS_SUCCESS;
+                        $this->getOrder()->setPaymentStatus(\XLite\Model\Order\Status\Payment::STATUS_PAID);
+                        $this->getOrder()->updateOrder();
+                        break;
+                    case "uncleared":
+                        $order_status = $transaction::STATUS_PENDING;
+                        //$order_status = $transaction::STATUS_SUCCESS;
+                        break;
+                    case "void":
+                        $order_status = $transaction::STATUS_VOID;
+                        break;
+                    case "declined":
+                        $order_status = \XLite\Model\Order\Status\Payment::STATUS_DECLINED;
+                        break;
+                    case "refunded":
+                        $this->getOrder()->setPaymentStatus(\XLite\Model\Order\Status\Payment::STATUS_REFUNDED);
+                        $this->getOrder()->updateOrder();
+                        $order_status = $transaction::STATUS_CANCELED;
+                        break;
+                    case "partial_refunded":
+                        $order_status = $transaction::STATUS_PENDING;
+                        $this->getOrder()->setPaymentStatus(\XLite\Model\Order\Status\Payment::STATUS_PART_PAID);
+                        $this->getOrder()->updateOrder();
+                        break;
+                    case "expired":
+                        $order_status = $transaction::STATUS_CANCELED;
+                        break;
+                    case "cancelled":
+                        $order_status = $transaction::STATUS_CANCELED;
+                        break;
+                    case "shipped":
+                        //don't do anything for status shipped.
+                        //$order_status = $transaction::STATUS_SUCCESS;
+                        break;
+                }
+
+                $this->transaction->setStatus($order_status);
+                $this->transaction->update();
+                if (\XLite\Core\Request::getInstance()->redirect == 'true') {
+                    if (\XLite\Core\Request::getInstance()->type == 'initial') {
+                        echo '<a href="' . $this->getReturnURL(null, true) . '&redirect=true&transactionid=' . \XLite\Core\Request::getInstance()->transactionid . '">Return to the webshop</a>';
+                        exit;
+                    } elseif (\XLite\Core\Request::getInstance()->cancel == '1') {
+                        $order_status = $transaction::STATUS_CANCELED;
+                        $this->transaction->setStatus($order_status);
+                    } else {
+                        header('Location:' . $this->getReturnURL(null, true) . '&redirect=true');
+                        echo 'OK';
+                        exit;
+                    }
+                } else {
+                    echo 'OK';
+                    exit;
+                }
+            }
+        } catch (Exception $e) {
+            
+        }
+    }
+
+    /**
+     * Get initial transaction type (used when customer places order)
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method object OPTIONAL
+     *
+     * @return string
+     */
+    public function getInitialTransactionType($method = null)
+    {
+        return \XLite\Model\Payment\BackendTransaction::TRAN_TYPE_SALE;
+    }
+
+    /**
+     * Check - payment method is configured or not
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return boolean
+     */
+    public function isConfigured(\XLite\Model\Payment\Method $method)
+    {
+        return parent::isConfigured($method) && $this->isAllSettingsProvided($method);
+    }
+
+    /**
+     * Check - payment method is configured or not
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return boolean
+     */
+    public function isAllSettingsProvided(\XLite\Model\Payment\Method $method)
+    {
+        return $method->getSetting('api_key');
+    }
+
+    /**
+     * Get return type
+     *
+     * @return string
+     */
+    public function getReturnType()
+    {
+        return self::RETURN_TYPE_HTTP_REDIRECT;
+    }
+
+    /**
+     * Returns the list of settings available for this payment processor
+     *
+     * @return array
+     */
+    public function getAvailableSettings()
+    {
+        return array(
+            'account_type',
+            'api_key',
+            'auto_redirect',
+            'days_active',
+            'ga_accountid',
+            'prefix'
+        );
+    }
+
+    /**
+     * Get payment method admin zone icon URL
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return string
+     */
+    public function getAdminIconURL(\XLite\Model\Payment\Method $method)
+    {
+        return true;
+    }
+
+    /**
+     * Check - payment method has enabled test mode or not
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return boolean
+     */
+    public function isTestMode(\XLite\Model\Payment\Method $method)
+    {
+        return '0' == $this->getSetting('Test');
+    }
+
+    /**
+     * Start payment transaction
+     * 
+     * @param type $issuerId
+     * @param type $transid
+     * @param type $settings
+     * @param string $gateway
+     */
+    public function startTransaction($issuerId = '', $transid, $settings = '', $gateway = '')
+    {
+        require_once LC_DIR_MODULES . 'MultiSafepay' . LC_DS . 'API' . LC_DS . 'Autoloader.php';
+
+        if (!$this->transaction && $transid) {
+            $this->transaction = \XLite\Core\Database::getRepo('XLite\Model\Payment\Transaction')->findOneByPublicTxnId($transid);
+        }
+
+        if ($settings != '') {
+            $this->settings = $settings;
+        }
+
+        $trans_type = "redirect";
+
+        if ($issuerId != '' && $gateway == 'IDEAL') {
+            $trans_type = "direct";
+        }
+
+        if ($gateway == 'BANKTRANS' && $this->getSetting('transaction_type') == '1') {
+            $trans_type = "direct";
+        }
+
+        if ($this->transaction) {
+            $orderId = $this->transaction->getPublicTxnId();
+            $settings = $this->getPaymentSettings($this->settings);
+
+            $items_list = '<ul>';
+            foreach ($this->getOrder()->getItems() as $item) {
+                $product = $item->getProduct();
+                $items_list.= '<li>' . $item->getAmount() . ' x ' . $product->getName() . '</li>';
+            }
+            $items_list .= '</ul>';
+
+            try {
+                $msp = new \MultiSafepayAPI\Client();
+                $msp->setApiKey($this->getSetting('api_key'));
+                $msp->setApiUrl($this->getEnvironment());
+                list($billing_street, $billing_housenumber) = $this->parseAddress($this->getProfile()->getBillingAddress()->getStreet());
+                list($shipping_street, $shipping_housenumber) = $this->parseAddress($this->getProfile()->getShippingAddress()->getStreet());
+
+                $msp->orders->post(array(
+                    "type" => $trans_type,
+                    "order_id" => $orderId,
+                    "currency" => strtoupper($this->getOrder()->getCurrency()->getCode()),
+                    "amount" => $this->getOrder()->getCurrency()->roundValue($this->transaction->getValue()) * 100,
+                    "gateway" => $gateway,
+                    "description" => $this->getInvoiceDescription(),
+                    "var1" => null,
+                    "var2" => null,
+                    "var3" => null,
+                    "items" => $items_list,
+                    "manual" => null,
+                    "days_active" => $this->getSetting('days_active'),
+                    "payment_options" => array(
+                        "notification_url" => $this->getReturnURL(null, true) . "&type=initial",
+                        "redirect_url" => $this->getReturnURL(null, true) . '&redirect=true',
+                        "cancel_url" => \XLite::getInstance()->getShopURL(\XLite\Core\Converter::buildURL('checkout'), \XLite\Core\Config::getInstance()->Security->customer_security
+                        ),
+                        "close_window" => false
+                    ),
+                    "customer" => array(
+                        "locale" => strtolower(\XLite\Core\Session::getInstance()->getLanguage()->getCode()),
+                        "ip_address" => $this->getClientIP(),
+                        "forwarded_ip" => $_SERVER['HTTP_X_FORWARDED_FOR'],
+                        "first_name" => $this->getProfile()->getBillingAddress()->getFirstname(),
+                        "last_name" => $this->getProfile()->getBillingAddress()->getLastname(),
+                        "address1" => $billing_street,
+                        "address2" => null,
+                        "house_number" => $billing_housenumber,
+                        "zip_code" => $this->getProfile()->getBillingAddress()->getZipcode(),
+                        "city" => $this->getProfile()->getBillingAddress()->getCity(),
+                        "state" => null,
+                        "country" => strtoupper($this->getProfile()->getBillingAddress()->getCountry()->getCode()),
+                        "phone" => $this->getProfile()->getBillingAddress()->getPhone(),
+                        "email" => $this->getProfile()->getLogin(),
+                        "disable_send_email" => false,
+                        "user_agent" => $_SERVER['HTTP_USER_AGENT'],
+                        "referrer" => $_SERVER['HTTP_REFERER']
+                    ),
+                    "delivery" => array(
+                        "first_name" => $this->getProfile()->getShippingAddress()->getFirstname(),
+                        "last_name" => $this->getProfile()->getShippingAddress()->getLastname(),
+                        "address1" => $shipping_street,
+                        "address2" => null,
+                        "house_number" => $shipping_housenumber,
+                        "zip_code" => $this->getProfile()->getShippingAddress()->getZipcode(),
+                        "city" => $this->getProfile()->getShippingAddress()->getCity(),
+                        "state" => null,
+                        "country" => strtoupper($this->getProfile()->getShippingAddress()->getCountry()->getCode()),
+                        "phone" => $this->getProfile()->getShippingAddress()->getPhone(),
+                        "email" => $this->getProfile()->getLogin()
+                    ),
+                    "gateway_info" => array(
+                        "issuer_id" => $issuerId,
+                        "birthday" => null,
+                        "bank_account" => null,
+                        "phone" => $this->getProfile()->getShippingAddress()->getPhone(),
+                        "email" => $this->getProfile()->getLogin(),
+                        "gender" => null,
+                        "referrer" => $_SERVER['HTTP_REFERER'],
+                        "user_agent" => $_SERVER['HTTP_USER_AGENT']
+                    ),
+                    "google_analytics" => array(
+                        "account" => $this->getSetting('ga_accountid')
+                    )
+                        //\XLite\Core\Config::getInstance()->Version->version,
+                ));
+
+                if ($trans_type == 'direct' && in_array($gateway, $this->directGateways())) {
+                    $url = \XLite\Core\Request::getInstance()->returnURL . '&redirect=true&transactionid=' . \XLite\Core\Request::getInstance()->transid;
+                } else {
+                    $url = $msp->orders->getPaymentLink();
+                }
+
+                header('Location: ' . $url);
+                exit;
+            } catch (\Exception $e) {
+                
+            }
+        }
+    }
+
+    /**
+     * Return the API Url based on the account type specified
+     * 
+     * @return string
+     */
+    protected function getEnvironment()
+    {
+        if ($this->getSetting('account_type') == '1') {
+            return "https://api.multisafepay.com/v1/json/";
+        } else {
+            return "https://testapi.multisafepay.com/v1/json/";
+        }
+    }
+
+    /**
+     * Array of type direct supported gateways
+     * 
+     * @return type array
+     */
+    protected function directGateways()
+    {
+        return array(
+            "BANKTRANS",
+            "DIRDEB",
+            "PAYPAL"
+        );
+    }
+
+    /**
+     * Get array of payment settings
+     *
+     * @return array
+     */
+    public function getPaymentSettings($settings)
+    {
+        $result = array();
+        $this->settings = $settings;
+
+        $fields = $this->getAvailableSettings();
+        foreach ($fields as $field) {
+            $result[$field] = $this->getSetting($field);
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get payment method setting by name
+     *
+     * @param string $name Setting name
+     *
+     * @result string
+     */
+    protected function getSetting($name)
+    {
+        $settings = $this->settings;
+        $result = parent::getSetting($name);
+
+        if (is_null($result)) {
+            $method = \XLite\Core\Database::getRepo('XLite\Model\Payment\Method')->findOneBy(array('service_name' => $this->settings));
+            $result = $method ? $method->getSetting($name) : null;
+        }
+
+        return $result;
+    }
+
+    /**
+     * Get redirect form URL
+     *
+     * @return string
+     */
+    protected function getFormURL()
+    {
+        return \XLite\Core\Converter::buildURL('connect', 'transaction');
+    }
+
+    /**
+     * Get redirect form fields list
+     *
+     * @return array
+     */
+    protected function getFormFields()
+    {
+        return array(
+            'transid' => $this->transaction->getPublicTxnId(),
+            'returnURL' => $this->getReturnURL(null, true),
+        );
+    }
+
+    /**
+     * Get input template
+     *
+     * @return string
+     */
+    public function getCheckoutTemplate(\XLite\Model\Payment\Method $method)
+    {
+        return 'modules/MultiSafepay/Connect/checkout/gateway.twig';
+    }
+
+    /**
+     * Get payment method icon path
+     * 
+     * @param type $order
+     * @param type $method
+     * @return type string
+     */
+    public function getIconPath(\XLite\Model\Order $order = null, \XLite\Model\Payment\Method $method = null)
+    {
+        return 'modules/MultiSafepay/' . $this->gateway . '/checkout/' . $this->icon;
+    }
+
+    /**
+     * Split the housenumber, suffix and street by provided address line
+     * 
+     * @param type $street_address
+     * @return type
+     */
+    public function parseAddress($street_address)
+    {
+        $address = $street_address;
+        $apartment = "";
+
+        $offset = strlen($street_address);
+
+        while (($offset = $this->rstrpos($street_address, ' ', $offset)) !== false) {
+            if ($offset < strlen($street_address) - 1 && is_numeric($street_address[$offset + 1])) {
+                $address = trim(substr($street_address, 0, $offset));
+                $apartment = trim(substr($street_address, $offset + 1));
+                break;
+            }
+        }
+
+        if (empty($apartment) && strlen($street_address) > 0 && is_numeric($street_address[0])) {
+            $pos = strpos($street_address, ' ');
+
+            if ($pos !== false) {
+                $apartment = trim(substr($street_address, 0, $pos), ", \t\n\r\0\x0B");
+                $address = trim(substr($street_address, $pos + 1));
+            }
+        }
+
+        return array($address, $apartment);
+    }
+
+    /**
+     * 
+     * @param type $haystack
+     * @param type $needle
+     * @param type $offset
+     * @return boolean
+     */
+    public function rstrpos($haystack, $needle, $offset = null)
+    {
+        $size = strlen($haystack);
+
+        if (is_null($offset)) {
+            $offset = $size;
+        }
+
+        $pos = strpos(strrev($haystack), strrev($needle), $size - $offset);
+
+        if ($pos === false) {
+            return false;
+        }
+
+        return $size - $pos - strlen($needle);
+    }
+
+}
---
+++ classes/XLite/Module/MultiSafepay/Directdebit/Model/Payment/Processor/Dirdeb.php
@@ -0,0 +1,221 @@
+<?php
+
+/**
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the MultiSafepay plugin
+ * to newer versions in the future. If you wish to customize the plugin for your
+ * needs please document your changes and make backups before you update.
+ *
+ * @category    MultiSafepay
+ * @package     Connect
+ * @author      TechSupport <techsupport@multisafepay.com>
+ * @copyright   Copyright (c) 2017 MultiSafepay, Inc. (http://www.multisafepay.com)
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+namespace XLite\Module\MultiSafepay\Directdebit\Model\Payment\Processor;
+
+class Dirdeb extends \XLite\Model\Payment\Base\WebBased
+{
+
+    /**
+     * Get operation types
+     *
+     * @return array
+     */
+    public function getOperationTypes()
+    {
+        return array(
+            self::OPERATION_SALE,
+        );
+    }
+
+    /**
+     * Get allowed backend transactions
+     *
+     * @return string Status code
+     */
+    public function getAllowedTransactions()
+    {
+        return array(
+            \XLite\Model\Payment\BackendTransaction::TRAN_TYPE_REFUND
+        );
+    }
+
+    /**
+     * Get settings widget or template
+     *
+     * @return string Widget class name or template path
+     */
+    public function getSettingsWidget()
+    {
+        return 'modules/MultiSafepay/Directdebit/config.twig';
+    }
+
+    /**
+     * Process return
+     *
+     * @param \XLite\Model\Payment\Transaction $transaction Return-owner transaction
+     *
+     * @return void
+     */
+    public function processReturn(\XLite\Model\Payment\Transaction $transaction)
+    {
+        parent::processReturn($transaction);
+        $processor = new \XLite\Module\MultiSafepay\Connect\Model\Payment\Processor\Connect();
+        $processor->settings = 'MultiSafepay Connect';
+        $processor->processReturn($transaction);
+    }
+
+    /**
+     * Get initial transaction type (used when customer places order)
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method object OPTIONAL
+     *
+     * @return string
+     */
+    public function getInitialTransactionType($method = null)
+    {
+        return \XLite\Model\Payment\BackendTransaction::TRAN_TYPE_SALE;
+    }
+
+    /**
+     * Check - payment method is configured or not
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return boolean
+     */
+    public function isConfigured(\XLite\Model\Payment\Method $method)
+    {
+        return parent::isConfigured($method) && $this->isAllSettingsProvided($method);
+    }
+
+    /**
+     * Check - payment method is configured or not
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return boolean
+     */
+    public function isAllSettingsProvided(\XLite\Model\Payment\Method $method)
+    {
+        return $method->getSetting('prefix');
+    }
+
+    /**
+     * Get return type
+     *
+     * @return string
+     */
+    public function getReturnType()
+    {
+        return self::RETURN_TYPE_HTTP_REDIRECT;
+    }
+
+    /**
+     * Returns the list of settings available for this payment processor
+     *
+     * @return array
+     */
+    public function getAvailableSettings()
+    {
+        return array(
+            'prefix'
+        );
+    }
+
+    /**
+     * Get payment method admin zone icon URL
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return string
+     */
+    public function getAdminIconURL(\XLite\Model\Payment\Method $method)
+    {
+        return true;
+    }
+
+    /**
+     * Check - payment method has enabled test mode or not
+     *
+     * @param \XLite\Model\Payment\Method $method Payment method
+     *
+     * @return boolean
+     */
+    public function isTestMode(\XLite\Model\Payment\Method $method)
+    {
+        return '0' == $this->getSetting('Test');
+    }
+
+    /**
+     * Start payment transaction
+     *
+     * @param string  $issuerId Issuer ID selected by customer
+     * @param integer $transid  Current transaction ID
+     *
+     * @return void
+     */
+    public function doTransactionRequest($issuerId, $transid)
+    {
+        $processor = new \XLite\Module\MultiSafepay\Connect\Model\Payment\Processor\Connect();
+        $processor->startTransaction('', $transid, 'MultiSafepay Connect', 'DIRDEB');
+    }
+
+    /**
+     * Get redirect form URL
+     *
+     * @return string
+     */
+    protected function getFormURL()
+    {
+        return \XLite\Core\Converter::buildURL('dirdeb', 'transaction');
+    }
+
+    /**
+     * Get redirect form fields list
+     *
+     * @return array
+     */
+    protected function getFormFields()
+    {
+        return array(
+            'transid' => $this->transaction->getPublicTxnId(),
+            'returnURL' => $this->getReturnURL(null, true)
+        );
+    }
+
+    /**
+     * 
+     * @param \XLite\Model\Order $order
+     * @param \XLite\Model\Payment\Method $method
+     * @return type
+     */
+    public function getIconPath(\XLite\Model\Order $order = null, \XLite\Model\Payment\Method $method = null)
+    {
+        $processor = new \XLite\Module\MultiSafepay\Connect\Model\Payment\Processor\Connect();
+        $processor->gateway = 'Directdebit';
+        $processor->icon = 'msp_directdebit.png';
+        return $processor->getIconPath($order, $method);
+    }
+
+    /**
+     * 
+     * @param \XLite\Model\Payment\Method $method
+     * @return string
+     */
+    public function getCheckoutTemplate(\XLite\Model\Payment\Method $method)
+    {
+        return 'modules/MultiSafepay/Connect/checkout/gateway.twig';
+    }
+
+}
