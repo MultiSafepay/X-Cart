8d86e7a4a56598b9dd22878e67c6a1e98c029edd
Jasper Nadi <26003384+Jasper-MultiSafepay@users.noreply.github.com>
2020-02-24 14:45:18 +0100

    PLGXCTS-100: [chore] Add release script
    * PLGXCTS-100: [chore] Add release script
    
    * PLGXCTS-100: [chore] Update gitattributes
    
    * PLGXCTS-100: [chore] Update gitignore

---
+++ bin/release.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Exit if any command fails
+set -eo pipefail
+
+RELEASE_VERSION=$1
+
+FILENAME_PREFIX="Plugin_X-Cart_"
+RELEASE_FOLDER=".dist"
+
+# If tag is not supplied, latest tag is used
+if [ -z "$RELEASE_VERSION" ]
+then
+  RELEASE_VERSION=$(git describe --tags --abbrev=0)
+fi
+
+# Remove old folder
+rm -rf "$RELEASE_FOLDER"
+
+# Create release
+mkdir "$RELEASE_FOLDER"
+git archive --format zip -9 --output "$RELEASE_FOLDER"/"$FILENAME_PREFIX""$RELEASE_VERSION".zip "$RELEASE_VERSION"
